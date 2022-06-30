450a0b26eefd011ba9ee23556b8bca3821f6846d
cuygur <cuygurdev@gmail.com>
2022-06-30 12:28:03 +0300

    define sizes object

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,27 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from '../../constants'
+import VisuallyHidden from '../VisuallyHidden'
+
+const SIZES = {
+  small: {
+    width: 370 + 'px',
+    '--height': 8 + 'px',
+  },
+  medium: {
+    width: 370 + 'px',
+    '--height': 12 + 'px',
+  },
+  large: {
+    width: 370 + 'px',
+    '--height': 24 + 'px',
+  },
+}
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
-};
+  return <strong>{value}</strong>
+}
 
-export default ProgressBar;
+export default ProgressBar
