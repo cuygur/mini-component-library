a60c4d899d1ff11a32dd4906474c1f97b5257635
cuygur <cuygurdev@gmail.com>
2022-06-30 13:13:08 +0300

    fiz sizes

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -7,21 +7,21 @@ import VisuallyHidden from '../VisuallyHidden'
 
 const SIZES = {
   small: {
-    width: 370 + 'px',
+    '--width': 370 + 'px',
     '--height': 8 + 'px',
   },
   medium: {
-    width: 370 + 'px',
+    '--width': 370 + 'px',
     '--height': 12 + 'px',
   },
   large: {
-    width: 370 + 'px',
+    '--width': 370 + 'px',
     '--height': 24 + 'px',
   },
 }
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>
+  return <div>{value}</div>
 }
 
 export default ProgressBar
