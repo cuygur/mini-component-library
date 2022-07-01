4349861199777f05857e779c7956c18e5f087a92
cuygur <cuygurdev@gmail.com>
2022-07-01 18:04:54 +0300

    add wrapper and loadbar components

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -9,19 +9,44 @@ const SIZES = {
   small: {
     '--width': 370 + 'px',
     '--height': 8 + 'px',
+    '--backgroundColor': COLORS.transparentGray15,
   },
   medium: {
     '--width': 370 + 'px',
     '--height': 12 + 'px',
+    '--backgroundColor': COLORS.transparentGray15,
   },
   large: {
     '--width': 370 + 'px',
     '--height': 24 + 'px',
+    '--backgroundColor': COLORS.transparentGray15,
   },
 }
 
 const ProgressBar = ({ value, size }) => {
-  return <div>{value}</div>
+  const styles = SIZES
+  console.log(styles.small)
+  const Wrapper = styled.div`
+    width: 370px;
+    height: 30px;
+    background-color: ${COLORS.transparentGray15};
+    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+    border-radius: 4px;
+    margin: 0 auto;
+  `
+  const LoadBar = styled.div`
+    width: 250px;
+    height: 30px;
+    background-color: ${COLORS.primary};
+    border-radius: 4px 0px 0px 4px;
+  `
+  return (
+    <>
+      <Wrapper style={styles}>
+        <LoadBar>{value}</LoadBar>
+      </Wrapper>
+    </>
+  )
 }
 
 export default ProgressBar
