a1d2b43ee10a0d710625b0aeb60b0161f36601c8
cuygur <cuygurdev@gmail.com>
2022-07-21 17:42:21 +0300

    iconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import { COLORS } from '../../constants';
+import { COLORS } from '../../constants'
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from '../Icon'
+import VisuallyHidden from '../VisuallyHidden'
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
-};
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  return (
+    <input
+      type='search'
+      placeHolder={placeholder}
+      label={label}
+      icon={icon}
+      size={size}
+      style={{ border: 'none', textDecorationLine: 'underline', width: width }}
+    ></input>
+  )
+}
 
-export default IconInput;
+export default IconInput
