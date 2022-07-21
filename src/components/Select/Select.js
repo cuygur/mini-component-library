c9d904ca029c1cd09da9abf5d883dfc0e65060be
cuygur <cuygurdev@gmail.com>
2022-07-21 13:44:50 +0300

    select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from '../../constants'
+import Icon from '../Icon'
+import { getDisplayedValue } from './Select.helpers'
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
-
+  const displayedValue = getDisplayedValue(value, children)
   return (
-    <select value={value} onChange={onChange}>
+    <select
+      value={value}
+      onChange={onChange}
+      style={{
+        width: 'fit-content',
+        blockSize: 'fit-content',
+        margin: '0',
+        padding: '0',
+        backgroundColor: 'yellow',
+        background: 'Arrow no-repeat right',
+      }}
+    >
       {children}
+      {Icon}
     </select>
-  );
-};
+  )
+}
 
-export default Select;
+export default Select
